beb19c6fc453acb4e50ccc1106b0db193da93a82
Jabarton <noblemonitor@gmail.com>
2022-05-15 00:28:32 +0500

    rework `SudokuSolver.solve` so it accepts puzzle as an argument

--- models/sudoku_solver.py
+++ models/sudoku_solver.py
@@ -6,46 +6,53 @@ from models.tech.single_candidate import SingleCandidate
 
 class SudokuSolver:
     def __init__(self):
-        self.puzzle = Puzzle.from_file('sudoku2.txt')
         self.tech = [
-            SingleCandidate(self.puzzle),
-            HiddenSingle(self.puzzle),
-            LockedCandidates(self.puzzle),
+            SingleCandidate,
+            HiddenSingle,
+            LockedCandidates,
         ]
 
-    def solve(self):
-        while not self.puzzle.is_solved():
+    def solve(self, puzzle: Puzzle) -> bool:
+        tech_instances = [tech(puzzle) for tech in self.tech]
+        is_validated = False
+
+        while not puzzle.is_solved():
             any_progress = False
 
-            for tech in self.tech:
+            for tech in tech_instances:
                 success = tech.apply()
                 any_progress = any_progress or success
 
             if not any_progress:
-                print('No progress detected, stopping the program')
+                print('No progress detected, stopping the solve')
                 break
 
-        self.give_breakdown()
+        self.give_breakdown(puzzle)
 
-        if self.puzzle.is_solved():
-            is_validated = self.puzzle.validate_solution()
+        if puzzle.is_solved():
+            is_validated = puzzle.validate_solution()
             if not is_validated:
                 print('Solution is invalid!\n')
 
-        self.display_puzzle()
+        self.display_puzzle(puzzle)
 
-    def give_breakdown(self):
-        current_cell_count = self.puzzle.count_cells()
-        total_cells = self.puzzle.size * self.puzzle.size
-        print(f'\nOriginal clue count: {self.puzzle.original_clue_count}')
-        print(f'Cells solved: {current_cell_count - self.puzzle.original_clue_count}')
+        return is_validated
+
+    @staticmethod
+    def give_breakdown(puzzle: Puzzle):
+        current_cell_count = puzzle.count_cells()
+        total_cells = puzzle.size * puzzle.size
+        print(f'\nOriginal clue count: {puzzle.original_clue_count}')
+        print(f'Cells solved: {current_cell_count - puzzle.original_clue_count}')
         print(f'Final progress: {(current_cell_count / total_cells):.0%}\n')
 
-    def display_puzzle(self):
-        for row in self.puzzle.grid:
+    @staticmethod
+    def display_puzzle(puzzle: Puzzle):
+        for row in puzzle.grid:
             print(row)
 
 
 if __name__ == '__main__':
     solver = SudokuSolver()
-    solver.solve()
+    p = Puzzle.from_file('sudoku.txt')
+    solver.solve(p)
